8947890b0a0a5757faa9693deceece3c78cc0bad
lst68868 <65935106+lst68868@users.noreply.github.com>
2023-07-11 11:50:45 -0400

    basic auth

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./styles/App.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/root";
+import { AuthProvider } from "./auth/Auth";
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <RouterProvider router={router} />
       <div>APP</div>
-    </>
+    </AuthProvider>
   );
 }
 
