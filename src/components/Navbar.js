3cf65f0c845cdc1106260963d503609ce9f8d9f4
lst68868 <65935106+lst68868@users.noreply.github.com>
2023-07-12 14:31:23 -0400

    NavBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import logo from "../images/logo.svg";
-import { AuthContext } from "../components/AuthContext";
+import { AuthContext } from "./AuthContext";
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
