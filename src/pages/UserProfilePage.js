6e33e502af6ff9d7eeb049406f74c0acad9e5c22
lst68868 <65935106+lst68868@users.noreply.github.com>
2023-07-13 09:53:40 -0400

    fix

--- src/pages/UserProfilePage.js
+++ src/pages/UserProfilePage.js
@@ -1,29 +1,28 @@
-import React from 'react'
-import UserCard from '../components/UserCard'
-import CoverPicture from '../images/Ethereum.png'
+import React from "react";
+import UserCard from "../components/UserCard";
+import CoverPicture from "../images/Ethereum.png";
 
 function UserProfilePage() {
   return (
     <>
-    <div>UserProfilePage</div>
-    <div>UserProfilePage</div>
-    <div>UserProfilePage</div>
-    <div>UserProfilePage</div>
-    <div>UserProfilePage</div>
-    <div>UserProfilePage</div>
-    <div>UserProfilePage</div>
-    <div>UserProfilePage</div>
-    <div>UserProfilePage</div>
-    <div>UserProfilePage</div>
-    <div>UserProfilePage</div>
-    <div>UserProfilePage</div>
+      <div>UserProfilePage</div>
+      <div>UserProfilePage</div>
+      <div>UserProfilePage</div>
+      <div>UserProfilePage</div>
+      <div>UserProfilePage</div>
+      <div>UserProfilePage</div>
+      <div>UserProfilePage</div>
+      <div>UserProfilePage</div>
+      <div>UserProfilePage</div>
+      <div>UserProfilePage</div>
+      <div>UserProfilePage</div>
+      <div>UserProfilePage</div>
+      <div className="user-profile">
+        <img className="cover-photo" src={CoverPicture} alt="" />
+        <UserCard />
+      </div>
     </>
-    <div className='user-profile'>
-      <img className='cover-photo' src={CoverPicture} alt="" />
-      <UserCard />
-    </div>
-
-  )
+  );
 }
 
-export default UserProfilePage
+export default UserProfilePage;
