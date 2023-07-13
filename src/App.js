4ed5f58d9a6d276d9374dee527da9393ec5d650e
LariMos <131423043+LariMos@users.noreply.github.com>
2023-07-13 09:43:22 -0400

    testing drop

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import "./styles/App.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/root";
 import { AuthProvider } from "./auth/Auth";
-import NavBar from "./components/Navbar";
+import NavBar from "./components/Navbar.js";
 import "./components/Footer.js";
-import Drop from "./components/Drop.js";
+// import Drop from "./components/Drop.js";
 import Trending from "./components/Trending";
 import Footer from "./components/Footer.js";
+import DropAnimation from "./components/DropAnimation";
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Drop />
+      {/* <Drop /> */}
+      <DropAnimation />
       <Trending />
       <Footer/>
       <AuthProvider>
--- src/components/Drop.js
+++ src/components/Drop.js
@@ -1,9 +1,17 @@
+
 import React from 'react'
 import drop from '../images/drop.svg'
 
 function Drop() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '70%', 
+      width: '80%', 
+      margin: '5% auto'
+     }}>
       <img
         src={drop}
         style={{ width: '100%', height: 'auto' }}
