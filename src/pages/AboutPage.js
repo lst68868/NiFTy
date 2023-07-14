2cee1e260cbd5a9f23b49b1992757c091383b409
nmahmood <mahmud.nafisa02@gmail.com>
2023-07-14 09:31:10 -0400

    about started

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -3,9 +3,10 @@ import React from 'react'
 function AboutPage() {
   return (
     <>
+      <div className='about'>
     <div>Our Story</div>
       <div> Our Team </div>
-      
+      </div>
       </>
   )
 }
