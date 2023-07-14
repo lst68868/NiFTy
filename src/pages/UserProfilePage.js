bc8b16e2c86113332d4d09f061d42f6be58b4a7c
lst68868 <65935106+lst68868@users.noreply.github.com>
2023-07-14 16:37:29 -0400

    bug

--- src/pages/UserProfilePage.js
+++ src/pages/UserProfilePage.js
@@ -3,10 +3,18 @@ import UserCard from "../components/UserCard";
 import CoverPicture from "../images/edit.jpg";
 
 function UserProfilePage() {
-  //TODO: Get the user's collected NFTs and created NFTs from the backend and replace the hard-coded values 
-  const collectedCards = ["https://cdn.lifestyleasia.com/wp-content/uploads/sites/2/2021/11/03175949/Bored-Ape-1.jpg", "https://i.seadn.io/gcs/files/69933fcc6791054a4262cfeb38460f05.gif?auto=format&dpr=1&w=1000", "https://dl.openseauserdata.com/cache/originImage/files/03b9d13915eef9f9d76e7bb17f22c59c.png", "https://dl.openseauserdata.com/cache/originImage/files/f5657ce1f1b3175ef825a87d3ac25803.png", "https://dl.openseauserdata.com/cache/originImage/files/ae06ceb52025299b4dece37d4c0980b2.png", "https://dl.openseauserdata.com/cache/originImage/files/6b9754fe84548b7a535013e0e55818f9.png", "https://dl.openseauserdata.com/cache/originImage/files/e39738085c521de5e437e71dd1172781.png"];
+  //TODO: Get the user's collected NFTs and created NFTs from the backend and replace the hard-coded values
+  const collectedCards = [
+    "https://cdn.lifestyleasia.com/wp-content/uploads/sites/2/2021/11/03175949/Bored-Ape-1.jpg",
+    "https://i.seadn.io/gcs/files/69933fcc6791054a4262cfeb38460f05.gif?auto=format&dpr=1&w=1000",
+    "https://dl.openseauserdata.com/cache/originImage/files/03b9d13915eef9f9d76e7bb17f22c59c.png",
+    "https://dl.openseauserdata.com/cache/originImage/files/f5657ce1f1b3175ef825a87d3ac25803.png",
+    "https://dl.openseauserdata.com/cache/originImage/files/ae06ceb52025299b4dece37d4c0980b2.png",
+    "https://dl.openseauserdata.com/cache/originImage/files/6b9754fe84548b7a535013e0e55818f9.png",
+    "https://dl.openseauserdata.com/cache/originImage/files/e39738085c521de5e437e71dd1172781.png",
+  ];
   const createdCards = [];
-  
+
   return (
     <>
       <div className="user-profile">
@@ -14,27 +22,26 @@ function UserProfilePage() {
         <UserCard />
       </div>
 
-      <div className='carousel-container'>
+      <div className="carousel-container">
         <h3>Collected NFTs</h3>
-        <div className='collected-nfts'>
+        <div className="collected-nfts">
           {collectedCards.map((_, i) => (
-            <NFTCardUserProfile key={i} imageSrc={collectedCards[i]}/>
+            <NFTCardUserProfile key={i} imageSrc={collectedCards[i]} />
           ))}
-          {collectedCards.length === 0 && 
+          {collectedCards.length === 0 && (
             <h2>This user doesn't own any NFTs</h2>
-          }
-        </div>
-          
-        <h3>Created NFTs</h3>
-        <div className='created-nfts'>
-          {createdCards.map((_, i) => (
-            <NFTCardUserProfile key={i} imageSrc={createdCards[i]}/>
-          ))}
-          {createdCards.length === 0 &&
-            <h2>This user hasn't created any NFTs</h2>
-          }
+          )}
         </div>
 
+        <h3>Created NFTs</h3>
+        <div className="created-nfts">
+          {createdCards.map((_, i) => (
+            <NFTCardUserProfile key={i} imageSrc={createdCards[i]} />
+          ))}
+          {createdCards.length === 0 && (
+            <h2>This user hasn't created any NFTs</h2>
+          )}
+        </div>
       </div>
     </>
   );
