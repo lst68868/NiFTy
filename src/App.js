7cc904de7ef3ce250db9ff8914a1a6bc037eee3b
lst68868 <65935106+lst68868@users.noreply.github.com>
2023-07-17 10:04:16 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./styles/App.css";
-import CreateNFT from "./components/CreateNFT";
-import { AuthProvider } from "./auth/Auth";
-import "./components/Footer.js";
-// import Drop from "./components/Drop.js";
-// import Footer from "./components/Footer.js";
-
 /*----- Components -----*/
-// import CreateNFT from "./components/CreateNFT";
+import CreateNFT from "./components/CreateNFT";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer.js";
-
 /*----- Pages -----*/
 import HomePage from "./pages/HomePage";
 import UserProfilePage from "./pages/UserProfilePage";
@@ -19,45 +12,22 @@ import NFTInfoPage from "./pages/NFTInfoPage";
 import SignUp from "./pages/SignUpPage";
 import AboutPage from "./pages/AboutPage";
 import SignInPage from "./pages/SignInPage";
-
 /*----- Utils -----*/
 import PrivateRoute from "./utils/PrivateRoute";
-
 /*----- Context -----*/
+// import { AuthContext } from "./context/AuthContext";
 function App() {
   return (
     <div>
       <NavBar />
-      {/* <Drop /> */}
-      {/* <div className="flex justify-center items-center h-[100vh] w-[100vw]"> */}
-      {/* <div className="anim-container">
-        <DropAnimation />
-      </div> */}
-      {/* <WaveAnimations /> */}
-      {/* </div> */}
-
-      {/* <WaveAnimations /> */}
-      {/* <Trending /> */}
-      {/* <Footer /> */}
-      {/* <CreateNFT /> */}
-      {/* <AuthProvider>
-        <RouterProvider router={router} />
-        <div></div>
-      </AuthProvider> */}
       <Routes>
         {/* Free Routes */}
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUp />} />
-        {isLoggedIn ? (
-          <Route path="/userprofile" element={<UserProfilePage />} />
-        ) : (
-          <Route path="/login" element={<SignInPage />} />
-        )}
         <Route path="/about" element={<AboutPage />} />
         <Route path="/" element={<HomePage />} />
-        <Route path='/nftinfo/:id' element={<NFTInfoPage />} />
-        <Route path="/userprofile" element={<UserProfilePage />} />
-
+        <Route path="/nftinfo/:id" element={<NFTInfoPage />} />
+        <Route path="/userprofile/:username" element={<UserProfilePage />} />
         {/* Auth Routes */}
         <Route exact element={<PrivateRoute />}>
           <Route path="/createnft" element={<CreateNFT />} />
@@ -67,5 +37,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,120 +4,59 @@ import { AuthContext } from "../context/AuthContext";
 import { useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { connectWallet } from "../web3files/walletConnection.js";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faWallet } from '@fortawesome/free-solid-svg-icons';
-
 function NavBar() {
-  const user = localStorage.getItem("user");
   const [isLoggedIn, setIsLoggedIn] = useState(
     localStorage.getItem("isLoggedIn")
   );
-
-  const { authTokens, user, logoutUser } = useContext(AuthContext);
+  const { authTokens, logoutUser } = useContext(AuthContext);
   const navigate = useNavigate();
-
   useEffect(() => {
     setIsLoggedIn(localStorage.getItem("isLoggedIn"));
   }, [authTokens]);
-
   const handleSignUp = (event) => {
     event.preventDefault();
     navigate("/signup");
   };
-
-  const handleUserProfile = () => {
-    if (!isLoggedIn) {
-      navigate("/signin");
-    } else {
-      navigate("/userprofile");
-    }
-  };
-
   return (
     <nav className="bg-slate-900 text-white flex items-center px-8">
       <div className="flex-grow">
         <a href="/">
-          <img
-            src={logo}
-            className="h-20 w-20"
-            alt="Mycelium logo"
-          />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "200px" }}
-            navbarScroll
-          >
-            <Nav.Link href="/drops">DROPS</Nav.Link>
-            <NavDropdown title="Collections" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="/collections/art">Art</NavDropdown.Item>
-              <NavDropdown.Item href="/collections/games">
-                Games
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/collections/domain">
-                Domain
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/collections/music">
-                Music
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/collections/profilepicture">
-                Profile Picture
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/collections/photography">
-                Photography
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="/collections/all">
-                All Collections
-              </NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title={<FontAwesomeIcon icon={faUser} />} id='navbarScrollingUserDropdown' style={{ color: '#212529' }}>
-              <NavDropdown.Item onClick={handleUserProfile}>Profile</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => connectWallet()}>
-                <FontAwesomeIcon icon={faWallet} /> Connect Wallet
-              </NavDropdown.Item>
-              <NavDropdown.Item href='/createnft'>Create NFT</NavDropdown.Item>
-              {isLoggedIn ? (
-                <NavDropdown.Item onClick={logoutUser}>SignOut</NavDropdown.Item>
-              ) : (
-                <>
-                  <NavDropdown.Item href='/signin'>Sign In</NavDropdown.Item>
-                  <NavDropdown.Item href='/signup' onClick={handleSignUp}>Sign Up</NavDropdown.Item>
-                </>
-              )}
-            </NavDropdown>
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search NFTs"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="dark">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+          <img src={logo} className="h-20 w-20" alt="Mycelium logo" />
         </a>
       </div>
       <div className="space-x-4">
-        <a href="/userprofile" className="text-neon-green hover:text-light-green transition-all duration-200">
+        <a
+          href="/userprofile"
+          className="text-neon-green hover:text-light-green transition-all duration-200"
+        >
           <i className="fas fa-user text-lg"></i>
         </a>
-        <button className="text-neon-green hover:text-light-green transition-all duration-200" onClick={() => connectWallet()}>
+        <button
+          className="text-neon-green hover:text-light-green transition-all duration-200"
+          onClick={() => connectWallet()}
+        >
           <i className="fas fa-wallet text-lg"></i>
         </button>
         {isLoggedIn ? (
-          <a className="text-neon-green hover:text-light-green transition-all duration-200" onClick={logoutUser}>
+          <a
+            className="text-neon-green hover:text-light-green transition-all duration-200"
+            onClick={logoutUser}
+          >
             SignOut
           </a>
         ) : (
           <div className="flex space-x-4">
-            <a href="/signin" className="text-neon-green hover:text-light-green transition-all duration-200">Sign In</a>
-            <a href="/signup" onClick={handleSignUp} className="text-neon-green hover:text-light-green transition-all duration-200">
+            <a
+              href="/signin"
+              className="text-neon-green hover:text-light-green transition-all duration-200"
+            >
+              Sign In
+            </a>
+            <a
+              href="/signup"
+              onClick={handleSignUp}
+              className="text-neon-green hover:text-light-green transition-all duration-200"
+            >
               Sign Up
             </a>
           </div>
@@ -126,5 +65,4 @@ function NavBar() {
     </nav>
   );
 }
-
 export default NavBar;
