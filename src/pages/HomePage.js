330b40f2579fea9c7b4174b2f6f3fc98e871d593
lst68868 <65935106+lst68868@users.noreply.github.com>
2023-07-17 12:40:20 -0400

    homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,18 +2,23 @@ import React from "react";
 import Trending from "../components/Trending";
 import NFTCarousel from "../components/NFTCarousel";
 import DropAnimation from "../components/DropAnimation";
-
+import Countdown from "../components/Countdown";
 function HomePage() {
   return (
-    <>
-      <div className="anim-container">
-        <DropAnimation />
+    <div className="bg-black">
+      <div className="relative bg-black w-full h-full grid sm:place-items-center">
+        <h1 className="absolute top-0 left-0 sm:static text-white text-2xl sm:text-4xl md:text-6xl font-orbitron z-10 p-2 sm:p-4 md:m-4">
+          FUTURE DROPS
+        </h1>
+        <DropAnimation className="absolute top-16 sm:inset-0 z-10 w-full h-full flex items-center justify-center" />
+        <div className="absolute inset-0 flex items-center justify-center z-20">
+          <Countdown />
+        </div>
       </div>
-      <NFTCarousel />
+      <NFTCarousel className="bg-black" />
       <Trending />
       <NFTCarousel />
-    </>
+    </div>
   );
 }
-
 export default HomePage;
