6bddf4b147caafd245877bb2382a819fc3b0e99f
nmahmood <mahmud.nafisa02@gmail.com>
2023-07-18 22:26:22 -0400

    fixed buttons and scaled

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -2,49 +2,20 @@ import React from "react";
 import Avatar from "@mui/material/Avatar";
 import ProfilePicture from "../images/NFT-Avatar.webp";
 
-
-function UserCard(props) {
+export default function UserCard(props) {
   return (
-    <div className="user-card" >
+    <div className="user-card">
       <div className="profile-pic-container">
-        <label htmlFor="profile-pic" >
+        <label htmlFor="profile-pic">
           <Avatar className="profile-pic" alt="Remy Sharp" src={ProfilePicture} />
         </label>
       </div>
       <div className="user-details" style={{ margin: '20px' }}>
-      <h2 className="username">{props.username}</h2>
-        <span>Ethereum Address</span>
-      </div>
-
-
-      {/* <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '1rem', padding: '0 1rem' }}>
-        <div style={{ display: 'flex' }}>
-          <ul className='list-unstyled mb-0' style={{ display: 'flex' }}>
-            <li style={{ marginRight: '1rem' }}>
-              <a href='#!' className='collected'>
-                Collected
-              </a>
-            </li>
-            <li style={{ marginRight: '1rem' }}>
-              <a href='#!' className='created'>
-                Created
-              </a>
-            </li>
-            <li style={{ marginRight: '1rem' }}>
-              <a href='#!' className='favorited'>
-                Favorited
-              </a>
-            </li>
-            <li>
-              <a href='#!' className='activity'>
-                Activity
-              </a>
-            </li>
-          </ul>
+        <div > 
+          <h2 className="username" style={{ fontSize: '2em', margin: '0' }}>{props.username}</h2> 
+          <span style={{ fontSize: '1.5em', display: 'block', margin: '0' }}>Ethereum Address</span> 
         </div>
-      </div> */}
+      </div>
     </div>
   );
 }
-
-export default UserCard;
